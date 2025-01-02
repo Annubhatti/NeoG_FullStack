a56e5c38388299b45f07cc9835906de5e1b11d03
annu bhatti <annujind207@gmail.com>
2025-01-02 12:44:57 +0530

    udated

--- 14. ReactJs/IR4.1_HW1/App.jsx
+++ 14. ReactJs/IR4.1_HW1/App.jsx
@@ -2,9 +2,10 @@ import "./App.css";
 import { useState } from "react";
 
 export default function App() {
-  const [counter, setCounter] = useState(0);
-
+  
   function ShowNumbers() {
+    const [counter, setCounter] = useState(0);
+
     return (
       <div>
         <p>{counter}</p>
@@ -15,17 +16,36 @@ export default function App() {
   }
 
   function BackgroundColorToggle() {
-    const [color, setColor] = useState(true);
+    const [color, setColor] = useState("lightblue");
 
     return (
       <div>
         <button
-          style={{ backgroundColor: color ? "lightblue" : "darkgrey" }}
-          onClick={() => setColor(!color)}
+          style={{ backgroundColor: color }}
+          onClick= {() => setColor((prevColor) => prevColor === "lightblue" ? "darkgrey" : "lightblue")}
         >
           Toggle Background
         </button>
       </div>
+    );
+  }
+
+  function ToggleText() {
+    const [text, setText] = useState("Hello, how are you?"); // Initial text state
+  
+    return (
+      <>
+        <p>{text}</p>
+        <button
+          onClick={() =>
+            setText((prevText) =>
+              prevText === "Hello, how are you?" ? "" : "Hello, how are you?"
+            )
+          }
+        >
+          {text === "Hello, how are you?" ? "Hide Message" : "Show Message"}
+        </button>
+      </>
     );
   }
 
@@ -57,17 +77,36 @@ export default function App() {
     );
   }
 
+  function ToggleColor() {
+    const [color, setBtnColor] = useState("green");
+
+    return (
+      <div>
+        <p style={{ Color : color}}>
+          This is Color toggleable text!
+        </p>
+        <button onClick={() =>setBtnColor((prevColor) => (prevColor === "red" ? "green" : "red" ))}>
+          Toggle Color
+        </button>
+      </div>
+    );
+  }
+
   function DisplayFruits() {
     
     const fruits = ["Apple", "Banana", "Orange", "Mango", "Grapes"];
-    return <div>
+    const [selectedFruit, setSelectedFruit] = useState("");
+    return(
+      <div>
       <h1>Select a Fruit</h1>
       <ul>
         {fruits.map((fruit, index) => (
-          <li key={index}>{fruit}</li>
+          <li key={index} onClick={() => setSelectedFruit(fruit)} style={{cursor: "pointer"}}>{fruit}</li>
         ))}
       </ul>
-    </div>;
+      {selectedFruit && (<p>You selected: <strong>{selectedFruit}</strong></p>)}
+    </div>
+    ) ;
   }
 
   return (
@@ -79,12 +118,16 @@ export default function App() {
       <BackgroundColorToggle />
 
       <hr />
+      <ToggleText />
+      <hr />
 
       <ShowMessage />
 
       <hr />
 
       <ToggleTextColor />
+      <hr />
+      <ToggleColor />
 
       <hr />
 
