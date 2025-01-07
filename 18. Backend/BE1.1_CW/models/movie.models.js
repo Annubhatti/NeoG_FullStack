76fc79175c4eba57ff6582deaa7193a49b15ed01
annu bhatti <annujind207@gmail.com>
2025-01-07 21:27:22 +0530

    BE

---
+++ 18. Backend/BE1.1_CW/models/movie.models.js
@@ -0,0 +1,75 @@
+const mongoose = require("mongoose");
+
+const movieSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    releaseYear: {
+      type: Number,
+      required: true,
+    },
+    genre: [
+      {
+        type: String,
+        enum: [
+          "Action",
+          "Comedy",
+          "Romance",
+          "Thriller",
+          "Fantasy",
+          "Sports",
+          "Musical",
+          "Horror",
+          "Drama",
+          "Sci-Fi",
+          "Others",
+        ],
+      },
+    ],
+    director: {
+      type: String,
+      required: true,
+    },
+    actors: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+    language: {
+      type: String,
+      required: true,
+    },
+    country: {
+      type: String,
+      default: "India",
+    },
+    rating: {
+      type: Number,
+      min: 0,
+      max: 10,
+      default: 0,
+    },
+    plot: {
+      type: String,
+    },
+    awards: {
+      type: String,
+    },
+    posterUrl: {
+      type: String,
+    },
+    trailerUrl: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Movie = mongoose.model("Movie", movieSchema);
+
+module.exports = Movie;
