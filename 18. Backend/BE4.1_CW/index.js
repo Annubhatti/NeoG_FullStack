7a72af26136a397529976ba11f6dd3b654e4a42d
annu bhatti <annujind207@gmail.com>
2025-01-09 18:26:37 +0530

    udated BE

---
+++ 18. Backend/BE4.1_CW/index.js
@@ -0,0 +1,111 @@
+const express= require("express");
+const app = express();
+
+const { initializeDatabase } = require("./db/db.connect");
+const Movie = require("./models/movies.models");
+
+app.use(express.json());
+
+initializeDatabase();
+
+//find a movie with a particular title
+async function  readMovieByTitle(movieTitle){
+  try {
+    const movie = await Movie.findOne({ title: movieTitle});
+    return movie;
+  } catch (error) {
+    throw error;
+  }
+};
+//readMovieByTitle("Lagaan")
+
+app.get("/movies/:title", async (req, res) =>{
+  try {
+    const movie = await readMovieByTitle(req.params.title)
+    if(movie){
+      res.json(movie)
+    }else{
+      res.status(404).json({error: "Movie not Found."})
+    }
+  } catch (error) {
+    res.status(500).json({error: "Failed to fetch movie."})
+  }
+});
+
+
+// to get all the movies in a database
+async function readAllMovies() {
+  try {
+    const allMovies = await Movie.find()
+    return allMovies
+  } catch (error) {
+    console.log(error)
+  }
+  
+}
+//readAllMovies();
+
+app.get("/movies",async(req, res) =>{
+  try {
+    const movies = await readAllMovies()
+    if(movies.length != 0){
+      res.json(movies)
+    }else{
+      res.status(404).json({error: "No movies found."})
+    }
+  } catch (error) {
+    res.status(500).json({error: "Failed to fetch movies."})
+  }
+})
+
+// get movie by director name
+async function readMovieByDirector(directorName) {
+  try {
+    const movieByDirector = await Movie.find({director: directorName})
+    return movieByDirector
+  } catch (error) {
+    console.log(error)
+  }
+  
+}
+//readMovieByDirector("Kabir Khan")
+
+app.get("/movies/director/:directorName", async (req, res) => {
+  try {
+    const movie = await readMovieByDirector(req.params.directorName);
+    if (movie) {
+      res.json(movie);
+    } else {
+      res.status(404).json({ error: "Movie By director not Found" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch movie by director" });
+  }
+});
+
+const readMoviesByGenre = async (genreName) => {
+  try {
+    const moviesByGenre = await Movie.find({ genre: genreName });
+    return moviesByGenre;
+  } catch (error) {
+    throw error;
+  }
+};
+
+app.get("/movies/genres/:genreName", async (req, res) => {
+  try {
+    const movies = await readMoviesByGenre(req.params.genreName);
+    if (movies.length > 0) {
+      res.json(movies);
+    } else {
+      res.status(404).json({ error: "Movies not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch movies" });
+  }
+});
+
+const PORT = 3000
+app.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`);
+});
